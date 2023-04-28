74519fa18bf371db2a2f8b4b42bf6c30b5d53830
Miguel Sosa <sosam3022@gmail.com>
2023-04-28 16:38:14 -0300

    arreglo de links css, js y img

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -8,21 +8,22 @@
 
   @yield('css')
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="/puntoventa/public/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="{{ asset('/plugins/fontawesome-free/css/all.min.css') }} " >
   <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" >
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="/puntoventa/public/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="{{ asset('/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }} " >
   <!-- iCheck -->
-  <link rel="stylesheet" href="/puntoventa/public/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="{{ asset('/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }} " >
   <!-- Theme style -->
-  <link rel="stylesheet" href="/puntoventa/public/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="{{ asset('/dist/css/adminlte.min.css') }} " >
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="/puntoventa/public/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="{{ asset('/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}" >
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="/puntoventa/public/plugins/daterangepicker/daterangepicker.css">
+  <link href="{{ asset('/plugins/daterangepicker/daterangepicker.css') }}" rel="stylesheet">
   <!-- summernote -->
-  <link rel="stylesheet" href="/puntoventa/public/plugins/summernote/summernote-bs4.min.css">
+  <link rel="stylesheet" href="{{ asset('/plugins/summernote/summernote-bs4.min.css') }} ">
+
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -30,7 +31,7 @@
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="/puntoventa/public/dist/img/bandera.png" alt="AdminLTELogo" height="160" width="180">
+      <img class="animation__shake" src="{{ asset('/dist/img/bandera.png') }}" alt="AdminLTELogo" height="160" width="180">
     </div>
 
     <!-- Navbar -->
@@ -64,7 +65,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="index3.html" class="brand-link">
-        <img src="/puntoventa/public/dist/img/bandera.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{{ asset('/dist/img/bandera.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">ADEOM colonia</span>
 
       </a>
@@ -222,34 +223,34 @@
     footer.textContent = year;
   </script>
   <!-- jQuery -->
-  <script src="/puntoventa/public/plugins/jquery/jquery.min.js"></script>
+  <script src="{{ asset('/plugins/jquery/jquery.min.js') }}"></script>
   <!-- jQuery UI 1.11.4 -->
-  <script src="/puntoventa/public/plugins/jquery-ui/jquery-ui.min.js"></script>
+  <script src="{{ asset('/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
   <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
   <script>
     $.widget.bridge('uibutton', $.ui.button)
   </script>
   <!-- Bootstrap 4 -->
-  <script src="/puntoventa/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{ asset('/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   <!-- ChartJS -->
-  <script src="/puntoventa/public/plugins/chart.js/Chart.min.js"></script>
+  <script src="{{ asset('/plugins/chart.js/Chart.min.js') }}"></script>
   <!-- Sparkline -->
-  <script src="/puntoventa/public/plugins/sparklines/sparkline.js"></script>
+  <script src="{{ asset('/plugins/sparklines/sparkline.js') }}"></script>
   <!-- jQuery Knob Chart -->
-  <script src="/puntoventa/public/plugins/jquery-knob/jquery.knob.min.js"></script>
+  <script src="{{ asset('/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
   <!-- daterangepicker -->
-  <script src="/puntoventa/public/plugins/moment/moment.min.js"></script>
-  <script src="/puntoventa/public/plugins/daterangepicker/daterangepicker.js"></script>
+  <script src="{{ asset('/plugins/moment/moment.min.js') }}"></script>
+  <script src="{{ asset('/plugins/daterangepicker/daterangepicker.js') }}"></script>
   <!-- Tempusdominus Bootstrap 4 -->
-  <script src="/puntoventa/public/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+  <script src="{{ asset('/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
   <!-- Summernote -->
-  <script src="/puntoventa/public/plugins/summernote/summernote-bs4.min.js"></script>
+  <script src="{{ asset('/plugins/summernote/summernote-bs4.min.js') }}"></script>
   <!-- overlayScropuntoventa/public/llbars -->
-  <script src="/puntoventa/public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <script src="{{ asset('/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
   <!-- AdminLTE App -->
-  <script src="/puntoventa/public/dist/js/adminlte.js"></script>
+  <script src="{{ asset('/dist/js/adminlte.js') }}"></script>
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-  <script src="/puntoventa/public/dist/js/pages/dashboard.js"></script>
+  <script src="{{ asset('/dist/js/pages/dashboard.js') }}"></script>
 
   @yield('js')
 
